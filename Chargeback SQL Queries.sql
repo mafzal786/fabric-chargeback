daeec1684758ace0d54f4eb25438dd29137549fa
Muhammad Afzal <afzal2000@yahoo.com>
2026-08-27 17:32:51 -0400

    Refactor SQL queries to use variables for dates

--- Chargeback SQL Queries.sql
+++ Chargeback SQL Queries.sql
@@ -1,8 +1,14 @@
 -- Sample Query to calculate CU and CU percentage from the raw data
 -- Get total capacity unit across all workspaces between the date range specified  
 declare @total_cu float
+declare @startdate DATETIME2
+declare @enddate DATETIME2
+
+set @startdate = '8/01/2026'
+set @enddate = '8/31/2026'
+  
 select @total_cu = sum([Chargeback_CU_s_]) from [dbo].[chargeback]
-where CONVERT(DATETIME2(0),[Chargeback_Date],103) BETWEEN '8/1/2026' AND '8/31/2026'
+where CONVERT(DATETIME2(0),[Chargeback_Date],103) BETWEEN @startdate AND @enddate
 
 --select @total_cu
 
@@ -14,6 +20,6 @@ from [dbo].[chargeback] cb
 join [dbo].[workspaces] w on w.Workspaces_Workspace_Id = cb.Chargeback_Workspace_Id
 join [dbo].[capacities] c on c.Capacities_Capacity_Id = cb.Chargeback_Capacity_Id
 -- where w.Workspaces_Workspace_name = 'DemoWS'
-and CONVERT(DATETIME2(0),[Chargeback_Date],103) BETWEEN '8/1/2026' AND '8/31/2026'
+and CONVERT(DATETIME2(0),[Chargeback_Date],103) BETWEEN @startdate AND @enddate
 --and c.[Capacities_Capacity_name] = 'fabric capacity'
 group by c.Capacities_Capacity_name, w.Workspaces_Workspace_name
