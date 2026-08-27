d6b5b995652f92b27b5026a2ea5d027eea4e7f27
Muhammad Afzal <afzal2000@yahoo.com>
2026-08-27 17:28:22 -0400

    Modify SQL queries for capacity unit calculations
    Updated date range for total capacity unit calculation and adjusted comments for clarity.

--- Chargeback SQL Queries.sql
+++ Chargeback SQL Queries.sql
@@ -1,24 +1,19 @@
-select w.[Workspaces_Workspace_name], c.[Capacities_Capacity_name],
-cb.[Chargeback_Capacity_Id], cb.[Chargeback_Workspace_Id], CONVERT(DATETIME2(0),[Chargeback_Date],103) as process_date,
-[Chargeback_CU_s_]
-from [dbo].[chargeback] cb
-join [dbo].[workspaces] w on w.Workspaces_Workspace_Id = cb.Chargeback_Workspace_Id
-join [dbo].[capacities] c on c.Capacities_Capacity_Id = cb.Chargeback_Capacity_Id
-where w.Workspaces_Workspace_name = 'DemoWS'
-and CONVERT(DATETIME2(0),[Chargeback_Date],103) BETWEEN '5/19/2026' AND '6/18/2026'
-
+-- Sample Query to calculate CU and CU percentage from the raw data
+-- Get total capacity unit across all workspaces between the date range specified  
 declare @total_cu float
 select @total_cu = sum([Chargeback_CU_s_]) from [dbo].[chargeback]
-where CONVERT(DATETIME2(0),[Chargeback_Date],103) BETWEEN '6/08/2026' AND '6/15/2026'
+where CONVERT(DATETIME2(0),[Chargeback_Date],103) BETWEEN '8/1/2026' AND '8/31/2026'
 
 --select @total_cu
 
+-- Get total CU and percent CU by capacity and workspaces
+  
 select sum([Chargeback_CU_s_]) as utilization,(sum([Chargeback_CU_s_])/@total_cu)*100 as utilization_percent,
 c.Capacities_Capacity_name, w.Workspaces_Workspace_name 
 from [dbo].[chargeback] cb
 join [dbo].[workspaces] w on w.Workspaces_Workspace_Id = cb.Chargeback_Workspace_Id
 join [dbo].[capacities] c on c.Capacities_Capacity_Id = cb.Chargeback_Capacity_Id
---where w.Workspaces_Workspace_name = 'DemoWS'
-and CONVERT(DATETIME2(0),[Chargeback_Date],103) BETWEEN '6/08/2026' AND '6/15/2026'
---and c.[Capacities_Capacity_name] = 'fabric4455eastus2'
+-- where w.Workspaces_Workspace_name = 'DemoWS'
+and CONVERT(DATETIME2(0),[Chargeback_Date],103) BETWEEN '8/1/2026' AND '8/31/2026'
+--and c.[Capacities_Capacity_name] = 'fabric capacity'
 group by c.Capacities_Capacity_name, w.Workspaces_Workspace_name
